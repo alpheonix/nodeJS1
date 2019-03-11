7e082fb56c712b891391e1e7c33074a308debd02
emmachlo <nassim.morouche1@gmail.com>
2019-03-11 14:55:43 +0100

    added POST request

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express();
 
+console.log('Hello Word');
+
 app.get("/hello", (req, res) => {
-  console.log("Responding to root...");
   res.send("Hello World");
 })
 
@@ -11,3 +12,7 @@ var port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Listening on port 3000...");
 })
+
+app.post('/chat', function (req, res) {
+  res.send(req.body[0]);
+});
