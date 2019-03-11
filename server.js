6ad525639e356ada06c1c28a40878a3c2152c53c
emmachlo <nassim.morouche1@gmail.com>
2019-03-11 15:50:04 +0100

    added POST fonctionnel

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 var express = require('express');
-
 var app = express();
+var port = process.env.PORT || 3000;
+app.use(express.json());
 
 console.log('Hello Word');
 
+app.post('/chat', (req, res) => {
+  switch(req.body.msg) {
+    case "ville" :
+      res.send("Nous sommes à Paris\n");
+      break;
+    case "météo" :
+      res.send("Il fait beau\n");
+      break;
+    default :
+      res.send("Réponse par défaut\n");
+      break;
+  }
+});
+
 app.get("/hello", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World\n");
 })
 
-var port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Listening on port 3000...");
 })
-
-app.post('/chat', function (req, res) {
-  res.send(req.body[0]);
-});
