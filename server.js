9d36a2cbdced4cd096eb7b04885e10cc7d8cc3de
emmachlo <nassim.morouche1@gmail.com>
2019-03-11 12:19:53 +0100

    installation serveur

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var express = require('express');
+
+var app = express();
+
+app.get("/helloworld", (req, res) => {
+  console.log("Responding to root...");
+  res.send("Hello World!");
+})
+
+app.listen(8080, () => {
+  console.log("Listening on port 8080...");
+})
